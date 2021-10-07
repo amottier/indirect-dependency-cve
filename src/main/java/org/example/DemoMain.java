7822e1277a8ae94a825675fdeaccfcf285d373d6
Antoine Mottier <antoine.mottier@ow2.org>
2021-10-07 19:48:38 +0200

    Initial commit

---
+++ src/main/java/org/example/DemoMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.IOException;
+
+public class DemoMain {
+    public static void main(String[] args) {
+
+    }
+
+    //https://github.com/spring-projects/spring-boot/blob/289ebef7da00852089db8520390f20d3a9228c6a/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
+}

2b8534c5cd50daa7aaee2c65f95696c4c8d49797
Antoine Mottier <antoine.mottier@ow2.org>
2021-10-12 19:09:43 +0200

    Test fasten.security risk verification

--- src/main/java/org/example/DemoMain.java
+++ src/main/java/org/example/DemoMain.java
@@ -1,11 +1,23 @@
 package org.example;
 
-import java.io.IOException;
+import io.grpc.netty.GrpcSslContexts;
+import io.netty.handler.codec.http.multipart.AbstractDiskHttpData;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
 
 public class DemoMain {
     public static void main(String[] args) {
-
+        GrpcSslContexts.configure(SslContextBuilder.forClient(), SslProvider.OPENSSL);
+        AbstractDiskHttpData
+        /*
+        Call trace:
+        GrpcSslContexts.configure grcp-netty
+        OpenSsl.isAlpnSupported() netty-handler
+        OpenSsl static block netty-handler
+        OpenSsl.loadTcNative() netty-handler
+        NativeLibraryLoader.loadFirstAvailable netty-common
+        NativeLibraryLoader.load netty-common (line 185 in version 4.1.52, line 180 in version 4.1.59
+         modified from `File.createTempFile` to `PlatformDependent.createTempFile`) as a fix to CVE-2021-21290
+         */
     }
-
-    //https://github.com/spring-projects/spring-boot/blob/289ebef7da00852089db8520390f20d3a9228c6a/spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
 }
